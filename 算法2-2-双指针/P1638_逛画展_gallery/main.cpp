a57e73d8229fd9d6c40b2c513b740d3da838c5c9
znzryb <znzryb34@gmail.com>
2024-08-30 17:30:31 +0800

    算法2-2双指针

---
+++ 算法2-2-双指针/P1638_逛画展_gallery/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+const int maxn=1e6+10;
+int n,m,pic[maxn];
+int main()
+{
+    cin>>n>>m;
+    for(int i=1;i<=n;i++) {
+        cin>>pic[i];
+    }
+    
+    return 0;
+}
